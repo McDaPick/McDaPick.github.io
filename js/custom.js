16b2e64e7b653a2e858012f2f6be71cd72e6c932
Michael Nielsen <michaelnielsen260@gmail.com>
2023-11-30 16:46:49 -0500

    testing body targetting

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 //This is a pen based off of Codewoofy's eyes follow mouse. It is just cleaned up, face removed, and then made to be a little more cartoony. https://codepen.io/Codewoofy/pen/VeBJEP
 $(document).ready(function() {
-  $(".move-area").mousemove(function(event) {
+  $("body").mousemove(function(event) {
     var eye = $(".eye");
     console.log('eye', eye)
     var x = (eye.offset().left) + (eye.width() / 2);
