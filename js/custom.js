c6ff612c6b2bb11ed89f5681772e4eb720f66358
Michael Nielsen <michaelnielsen260@gmail.com>
2025-01-07 16:55:46 -0500

    Alex test

--- js/custom.js
+++ js/custom.js
@@ -14,4 +14,9 @@ $(document).ready(function() {
       'transform': 'rotate(' + rot + 'deg)'
     });
   });
+
+  $(".square").on("mouseover", function () {
+    $("#testimage").attr("src","https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.espn.com%2Fespn%2Fphotos%2Fgallery%2F_%2Fid%2F13525153%2Fimage%2F8%2Findustry-moves-gallery-being-rodney-mullen&psig=AOvVaw3ORqsPge_o68Exh_qpF098&ust=1736373269540000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCcuNLM5IoDFQAAAAAdAAAAABAE");
+    $("#testtext").text("YOOOO ALEX");
+  });
 });
